jhnc-oss/glibc

commit 79b2667d1eb06c6503c22f2f323c1c574ac5917b

time: strftime_l: Avoid an unbounded alloca.
Avoid possible stack overflow by removing alloca() and converting to
wide characters within the buffer.

Suggested-by: Paul Eggert <eggert@cs.ucla.edu>

time/strftime_l.c
@@ -267,15 +267,6 @@ static const CHAR_T zeroes[16] = /* "0000000000000000" */
 #  undef __mbsrtowcs_l
 #  define __mbsrtowcs_l(d, s, l, st, loc) __mbsrtowcs (d, s, l, st)
 # endif
-# define widen(os, ws, l) \
-  {									      \
-    mbstate_t __st;							      \
-    const char *__s = os;						      \
-    memset (&__st, '\0', sizeof (__st));				      \
-    l = __mbsrtowcs_l (NULL, &__s, 0, &__st, loc);			      \
-    ws = alloca ((l + 1) * sizeof (wchar_t));				      \
-    (void) __mbsrtowcs_l (ws, &__s, l, &__st, loc);			      \
-  }
 #endif
 
 
@@ -1342,11 +1333,31 @@ __strftime_internal (CHAR_T *s, size_t maxsize, const CHAR_T *format,
 #ifdef COMPILE_WIDE
 	  {
 	    /* The zone string is always given in multibyte form.  We have
-	       to transform it first.  */
-	    wchar_t *wczone;
-	    size_t len;
-	    widen (zone, wczone, len);
-	    cpy (len, wczone);
+               to convert it to wide character.  */
+            size_t w = pad == L_('-') || width < 0 ? 0 : width;
+            char const *z = zone;
+            mbstate_t st = {0};
+            size_t len = __mbsrtowcs_l (p, &z, maxsize - i, &st, loc);
+            if (len == (size_t) -1)
+              return 0;
+            size_t incr = len < w ? w : len;
+            if (incr >= maxsize - i)
+              {
+                errno = ERANGE;
+                return 0;
+              }
+            if (p)
+              {
+                if (len < w)
+                  {
+                    size_t delta = w - len;
+                    __wmemmove (p + delta, p, len);
+                    wchar_t wc = pad == L_('0') || pad == L_('+') ? L'0' : L' ';
+                    wmemset (p, wc, delta);
+                  }
+                p += incr;
+              }
+            i += incr;
 	  }
 #else
 	  cpy (strlen (zone), zone);

commit c6fd4ceffaf0b1d85cba0e97f83855849c5e21d4

time: handle ^ and # modifiers with %Z in wcsftime (bug 34611)
Fixes a regression in 79b2667d1eb06 ("time: strftime_l: Avoid an unbounded
alloca").

time/strftime_l.c
@@ -1352,10 +1352,19 @@ __strftime_internal (CHAR_T *s, size_t maxsize, const CHAR_T *format,
                 if (len < w)
                   {
                     size_t delta = w - len;
-                    __wmemmove (p + delta, p, len);
+                    if (to_lowcase)
+		      memcpy_lowcase (p + delta, p, len LOCALE_ARG);
+		    else if (to_uppcase)
+		      memcpy_uppcase (p + delta, p, len LOCALE_ARG);
+		    else
+		      __wmemmove (p + delta, p, len);
                     wchar_t wc = pad == L_('0') || pad == L_('+') ? L'0' : L' ';
                     wmemset (p, wc, delta);
                   }
+		else if (to_lowcase)
+		  memcpy_lowcase (p, p, len LOCALE_ARG);
+		else if (to_uppcase)
+		  memcpy_uppcase (p, p, len LOCALE_ARG);
                 p += incr;
               }
             i += incr;

time/tst-ftime_l.c
@@ -1,3 +1,4 @@
+#include <libc-diag.h>
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -121,6 +122,44 @@ do_test (void)
     }
   else
     printf ("got \"%s\"\n", buf);
+
+  tm.tm_zone = "Zz";
+
+  /* GCC complains about using a width below with %Z, but it is supported
+     in the glibc implementation.  */
+  DIAG_PUSH_NEEDS_COMMENT;
+  DIAG_IGNORE_NEEDS_COMMENT (0, "-Wformat");
+
+  if (strftime (buf, sizeof (buf), "%Z %^Z %#Z %^4Z %#4Z", &tm) == 0)
+    {
+      puts ("strftime failed");
+      result = 1;
+    }
+  else if (strcmp (buf, "Zz ZZ zz   ZZ   zz") != 0)
+    {
+      printf ("strftime: expected \"%s\", got \"%s\"\n",
+	      "Zz ZZ zz   ZZ   zz", buf);
+      result = 1;
+    }
+  else
+    printf ("got \"%s\"\n", buf);
+
+  if (wcsftime (wbuf, sizeof (wbuf) / sizeof (wbuf[0]),
+		L"%Z %^Z %#Z %^4Z %#4Z", &tm) == 0)
+    {
+      puts ("wcsftime failed");
+      result = 1;
+    }
+  else if (wcscmp (wbuf, L"Zz ZZ zz   ZZ   zz") != 0)
+    {
+      printf ("wcsftime: expected \"%ls\", got \"%ls\"\n",
+	      L"Zz ZZ zz   ZZ   zz", wbuf);
+      result = 1;
+    }
+  else
+    printf ("got \"%ls\"\n", wbuf);
+
+  DIAG_POP_NEEDS_COMMENT;
 
   return result;
 }